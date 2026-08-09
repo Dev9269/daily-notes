ab3a1a5cb6e0733fd1eb174d6bc8fda89e33a2f1
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2026-08-09 13:53:16 +0530

    feat(scripts): banner truncation

--- scripts/banner.py
+++ scripts/banner.py
@@ -1,3 +1,7 @@
 def box(text, w=60):
     border = '#' * w
-    return '\n'.join([border, text.center(w), border])
+    title = text.center(w)
+    return f'{border}\n{title}\n{border}'
+
+def shrink(text, w=60):
+    return text[:w-3] + '...' if len(text) > w else text
