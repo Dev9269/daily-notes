5bd382a34e26d226a3e472790ced4f0080817542
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2026-08-09 13:53:16 +0530

    feat(scripts): git_ops uptodate check

--- scripts/git_ops.py
+++ scripts/git_ops.py
@@ -6,3 +6,6 @@ def sh(*args):
 
 def short_log(n=10):
     return sh('git', 'log', '--oneline', f'-{n}')
+
+def uptodate(remote='origin', branch='main'):
+    return sh('git', 'rev-parse', f'{remote}/{branch}')
