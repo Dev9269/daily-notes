206d8eaee849cdd0465d84819bb0b7be3af11863
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2026-08-09 13:53:16 +0530

    feat(scripts): random_ip + random_mac

--- scripts/random_ip.py
+++ scripts/random_ip.py
@@ -1,7 +1,7 @@
 import random
 
-def rand_octet():
-    return random.randint(0, 255)
-
 def random_ip():
-    return '.'.join(str(randon()) for _ in range(4))
+    return '.'.join(str(random.randint(0, 255)) for _ in range(4))
+
+def random_mac():
+    return ':'.join(f'{random.randint(0,255):02x}' for _ in range(6))
