73a73206fb8237b3d149da8fe4e7191f497de500
Jainam maru <157703342+Dev9269@users.noreply.github.com>
2026-08-10 11:35:00 +0000

    feat(scripts): net_iface is_private check

--- scripts/net_iface.py
+++ scripts/net_iface.py
@@ -3,3 +3,6 @@ import socket
 def local_ips():
     hostname = socket.gethostname()
     return socket.gethostbyname_ex(hostname)[2]
+
+def is_private(ip):
+    return ip.startswith(('10.', '192.168.')) or ip.startswith('172.') and 16 <= int(ip.split('.')[1]) <= 31
